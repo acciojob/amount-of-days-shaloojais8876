adae6be41931b020a668e975a215a9fecaded438
shaloojais8876 <104994730+shaloojais8876@users.noreply.github.com>
2024-08-05 21:09:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function daysOfAYear(year){
+	let isLeap = false;
+	if(year 400 % === 0) {
+		//this may be leap year
+		if(year % 100 === 0) {
+			//century year
+			 return year % 400 === 0 ? 366 : 365;
+		}
+		return 366;
+	}
+}
